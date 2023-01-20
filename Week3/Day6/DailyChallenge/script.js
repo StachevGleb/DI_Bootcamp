279ba1c327c025e5f6ee6315ed3ff84783f71950
Glebcode <gleb.stachev@gmail.com>
2023-01-20 20:58:31 +0200

    week3Day6DailyCh

---
+++ Week3/Day6/DailyChallenge/script.js
@@ -0,0 +1,13 @@
+let astr = "*"
+// for(let i = 0;i < 7;i++){
+//  console.log(astr.repeat(i))
+// }
+
+for(let i = 0;i < 6;i++){
+    let quantity = i + 1; 
+    let str = "";
+for(let j = 0;j < quantity;j++){
+str = str +"*"
+    }
+    console.log(str)
+}
