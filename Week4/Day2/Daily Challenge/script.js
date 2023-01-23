b1fc55a5b7eebd3af13e2db2116f819761a3207d
Glebcode <gleb.stachev@gmail.com>
2023-01-24 00:14:20 +0200

    week4Day2DailyCh

---
+++ Week4/Day2/Daily Challenge/script.js
@@ -0,0 +1,23 @@
+let arr = prompt("Insert some words separated by comma - ',' ")
+let array = arr.split(",")
+
+for(let i = 0; i < (array.length-1);i++){
+if(array[i].length > array[i+1].length){
+    let astrCount= array[i].length
+    // console.log(array[i].length)
+} else{
+    astrCount = array[i+1].length
+    // console.log(array[i+1].length)
+}
+let astrArr =[]
+    // console.log(array[i])
+}
+let astrArr = Array(astrCount+4).fill("*");
+console.log(astrArr.join(""))
+
+for(let j = 0; j < array.length;j++){
+console.log("* " + array[j] +  " *")
+}
+console.log(astrArr.join(""))
+
+// console.log(array)
