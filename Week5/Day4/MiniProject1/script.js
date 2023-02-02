0cb5536172d438e1a40360c35d35c17aa9114804
Glebcode <gleb.stachev@gmail.com>
2023-02-02 15:44:55 +0200

    week5Day4MiniProj

---
+++ Week5/Day4/MiniProject1/script.js
@@ -0,0 +1,52 @@
+generateCollors()
+listenToMain()
+let currentColor
+let isMousePressed = false
+let chosenColor
+
+function generateCollors(){
+    let colorDivs = document.querySelectorAll(".color")
+    let color = generateRandomColor()
+    for(let colorDiv of colorDivs){
+        let color = generateRandomColor()
+        colorDiv.style.backgroundColor = color
+        colorDiv.addEventListener("click", setCurrentColor)
+        colorDiv.dataset.color = color
+    }
+}
+
+function generateRandomColor(){
+    let hex = Math.floor(Math.random() * 16777215).toString(16)
+    return "#" + hex
+}
+
+function setCurrentColor(e){
+ currentColor = e.target.dataset.color
+ chosenColor = currentColor
+ let but = document.querySelector(".clear button")
+ but.style.backgroundColor = chosenColor
+}
+
+function listenToMain(){
+    let main = document.getElementById("main")
+    main.addEventListener("mousedown", handleClick)
+    main.addEventListener("mousemove", handleMove)
+    document.body.addEventListener("mouseup", handleUp)
+}
+
+
+function handleClick(){
+if(currentColor == null) return
+console.log(currentColor);
+isMousePressed = true
+}
+
+function handleMove(e){
+if(!isMousePressed) return
+console.log("cordinates: ", e.x, e.y);
+let currentDiv = document.elementFromPoint(e.x, e.y)
+currentDiv.style.backgroundColor = currentColor
+}
+function handleUp(){
+    isMousePressed = false
+}
