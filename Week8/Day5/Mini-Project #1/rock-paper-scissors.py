a3878ab70ac321d8f8c5d468df75c41aad197d8d
Glebcode <gleb.stachev@gmail.com>
2023-02-26 16:05:53 +0200

    Week8Day5MiniPro

--- Week8/Day5/Mini-Project #1/rock-paper-scissors.py
+++ Week8/Day5/Mini-Project #1/rock-paper-scissors.py
@@ -5,27 +5,27 @@ def get_user_menu_choice():
     print("(x) Show scores and exit:  ")
     user_menu_choice = input(": ")
     checker = True
+    result = []
     while checker:
         if user_menu_choice == 'x':
+            checker = False
             pass
         elif user_menu_choice == 'g':
             test = Game()
             res = test.play()
-            print(print_results(res))
-            res[res] += 1
+            result.append(res)
+            print_results(result)
         else:
             print("Menu:")
             print("(g) Play a new game:  ")
             print("(x) Show scores and exit:  ")
             user_menu_choice = input(": ")
-def print_results(res):
+def print_results(result):
     print("Results:")
-    print(f'Winner - {res["win"]} times, Looser - {res["loss"]} times, Drews - {res["draw"]} times')
-
+    print(f'Winner - {result.count("win")} times, Looser - {result.count("loss")} times, Drews - {result.count("draw")} times')
+    print(f"Thank you for playing.")
 def main():
     get_user_menu_choice()
-
-
 
 main()
 
