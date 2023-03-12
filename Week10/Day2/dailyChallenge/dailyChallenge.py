e2edee3b9f32dc813d52f7bf662c0b950b347c03
Glebcode <gleb.stachev@gmail.com>
2023-03-12 17:40:34 +0200

    Week10Day2DailyCh

--- Week10/Day2/dailyChallenge/dailyChallenge.py
+++ Week10/Day2/dailyChallenge/dailyChallenge.py
@@ -1,1 +1,8 @@
-french_words= ["Bonjour", "Au revoir", "Bienvenue", "A bientôt"]
+from deep_translator import GoogleTranslator
+
+french_words = ["Bonjour", "Au revoir", "Bienvenue", "A bientôt"]
+en_sentense = {}
+
+for word in french_words:
+    en_sentense[word] = GoogleTranslator(source='auto', target='en').translate(word)
+print(en_sentense)
