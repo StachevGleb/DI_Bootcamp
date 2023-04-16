18ed87a1dfd7d989eb348cd0b0352e9337f995ac
Glebcode <gleb.stachev@gmail.com>
2023-04-16 15:21:37 +0300

    Week12Day2DailyCh

--- Week12/Day2/DailyChallenge/app.py
+++ Week12/Day2/DailyChallenge/app.py
@@ -1,15 +1,17 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, TextAreaField
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret_key_here'
 
+
 class CVForm(FlaskForm):
     first_name = StringField('First Name')
     last_name = StringField('Last Name')
     age = IntegerField('Age')
     experience = TextAreaField('Experience')
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -21,6 +23,7 @@ def index():
         experience = form.experience.data
         return render_template('my_cv.html', first_name=first_name, last_name=last_name, age=age, experience=experience)
     return render_template('index.html', form=form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
