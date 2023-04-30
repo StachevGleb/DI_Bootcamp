62b18d583b3239b6bb025f920b14df36828c1221
Glebcode <gleb.stachev@gmail.com>
2023-04-30 18:06:23 +0300

    Week13Day2MiniProj

--- Week13/Day2/app/models.py
+++ Week13/Day2/app/models.py
@@ -1,6 +1,6 @@
 from app import db
-#for
- User(db.Model):
+
+class User(db.Model):
     user_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64))
     street = db.Column(db.String(64))
